31c3c653ea1f52952f1f8e8999043f7a9941897e
d3m0li5h3r <demolisherrules@gmail.com>
2022-12-04 22:07:12 -0800

    add Day 5

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,15 +1,102 @@
+import java.util.*
+
+data class Instruction(val count: Int, val from: Int, val to: Int)
+
+data class Parsed(val stacks: List<Stack<Char>>, val instruction: List<Instruction>)
+
 fun main() {
+    fun parse(lines: List<String>): Parsed {
+        var containers = ""
+        var stackCount: Int = 0
+        var startIndex: Int = 0
+        for (index in lines.indices) {
+            val normalized = lines[index].replace("    ", " - ")
+            if (normalized.trim().startsWith("1")) {
+                startIndex = index + 2
+                stackCount = normalized.trim().last() - '0'
+
+                break
+            }
+
+            containers += "\n ${normalized.trim()}"
+        }
+
+        val stacks = mutableListOf<Stack<Char>>()
+
+        for (index in 0 until stackCount) {
+            stacks.add(Stack())
+        }
+
+        containers
+            .split("\n")
+            .filter { it.isNotBlank() }
+            .forEach { column ->
+                column.split(" ")
+                    .filter { it.isNotBlank() }
+                    .forEachIndexed { index, s ->
+                        if (s != "-") {
+                            stacks[index].push(s.replace("[", "").replace("]", "").first())
+                        }
+                    }
+            }
+
+        stacks.forEach {
+            it.reverse()
+        }
+
+        val instructions = mutableListOf<Instruction>()
+
+        for (index in startIndex until lines.size) {
+            val instruction = lines[index]
+                .split(" ")
+                .filter { it.toIntOrNull() != null }
+
+            instructions.add(
+                Instruction(
+                    instruction[0].toInt(),
+                    instruction[1].toInt() - 1,
+                    instruction[2].toInt() - 1
+                )
+            )
+        }
+
+        return Parsed(stacks, instructions)
+    }
+
+    fun topOfAllStacks(stacks: List<Stack<Char>>): String =
+        stacks.map { it.peek() }.joinToString("")
+
     fun part1(lines: List<String>): String {
-        return ""
+        val (stacks, instructions) = parse(lines)
+
+        instructions.forEach {
+            for (index in 0 until it.count) {
+                stacks[it.to].push(stacks[it.from].pop())
+            }
+        }
+
+        return topOfAllStacks(stacks)
     }
 
     fun part2(lines: List<String>): String {
-        return ""
+        val (stacks, instructions) = parse(lines)
+
+        instructions.forEach {
+            val popStack = Stack<Char>()
+            for (index in 0 until it.count) {
+                popStack.push(stacks[it.from].pop())
+            }
+            while(!popStack.empty()) {
+                stacks[it.to].push(popStack.pop())
+            }
+        }
+
+        return topOfAllStacks(stacks)
     }
 
     val testInput = readInputLines("Day05_test")
-    check(part1(testInput) == "")
-    check(part2(testInput) == "")
+    check(part1(testInput) == "CMZ")
+    check(part2(testInput) == "MCD")
 
     val input = readInputLines("Day05")
     println(part1(input))
---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main() {
+    fun part1(lines: List<String>): String {
+        return ""
+    }
+
+    fun part2(lines: List<String>): String {
+        return ""
+    }
+
+    val testInput = readInputLines("Day06_test")
+    check(part1(testInput) == "")
+    check(part2(testInput) == "")
+
+    val input = readInputLines("Day06")
+    println(part1(input))
+    println(part2(input))
+}
