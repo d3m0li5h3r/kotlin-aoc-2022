e29a1f277afdbcae112abd1b90679442ab5b1137
d3m0li5h3r <demolisherrules@gmail.com>
2022-12-07 13:13:50 -0800

    add Day 7

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,17 +1,46 @@
 fun main() {
-    fun part1(line: String): String {
-        return ""
+    fun sizes(lines: List<String>): Map<String, Int> = buildMap {
+        put("", 0)
+
+        var pwd = ""
+
+        for (line in lines) {
+            if (line.startsWith("$ cd")) {
+                pwd = when (val dir = with(line.split(" ")) { get(lastIndex) }) {
+                    "/" -> ""
+                    ".." -> pwd.substringBeforeLast("/", "")
+                    else -> if (pwd.isEmpty()) dir else "$pwd/$dir"
+                }
+            } else if (line[0].isDigit()) {
+                val size = line.split(" ")[0].toInt()
+
+                var dir = pwd
+                while (true) {
+                    this[dir] = (this[dir] ?: 0) + size
+                    if (dir.isEmpty()) break
+                    dir = dir.substringBeforeLast("/", "")
+                }
+            }
+        }
     }
 
-    fun part2(line: String): String {
-        return ""
+    fun part1(lines: List<String>): String {
+        return sizes(lines).values.filter { it <= 100000 }.sum().toString()
     }
 
-    val testInput = readInputString("Day07_test")
-    check(part1(testInput) == "")
-    check(part2(testInput).also { println(it) } == "")
+    fun part2(lines: List<String>): String {
+        val sizes = sizes(lines)
 
-    val input = readInputString("Day07")
+        val availableSpace = 70000000 - (sizes[""] ?: 0)
+
+        return sizes.values.filter { availableSpace + it >= 30000000 }.min().toString()
+    }
+
+    val testInput = readInputLines("Day07_test")
+    check(part1(testInput) == "95437")
+    check(part2(testInput) == "24933642")
+
+    val input = readInputLines("Day07")
     println(part1(input))
     println(part2(input))
 }
---
+++ src/main/kotlin/Day8.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(lines: List<String>): String {
+        return ""
+    }
+
+    fun part2(lines: List<String>): String {
+        return ""
+    }
+
+    val testInput = readInputLines("Day08_test")
+    check(part1(testInput) == "")
+    check(part2(testInput) == "")
+
+    val input = readInputLines("Day08")
+    println(part1(input))
+    println(part2(input))
+}
